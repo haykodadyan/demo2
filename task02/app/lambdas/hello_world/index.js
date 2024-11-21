eac7a33ccd42670cebc5312c0e5833118d6d90b7
Hayk Dadyan <hayk_dadyan@epam.com>
2024-11-22 00:20:03 +0400

    second commit

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,8 +1,21 @@
 exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-        body: JSON.stringify('Hello from Lambda!'),
+    const { path, httpMethod } = event;
+
+    if (path === "/hello" && httpMethod === "GET") {
+        return {
+            statusCode: 200,
+            body: JSON.stringify({
+                statusCode: 200,
+                message: "Hello from Lambda",
+            }),
+        };
+    }
+
+    return {
+        statusCode: 400,
+        body: JSON.stringify({
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
+        }),
     };
-    return response;
 };
