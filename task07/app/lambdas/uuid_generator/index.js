188307ae7f86a058f7c8560652a7c55d32cc3192
Hayk Dadyan <hayk_dadyan@epam.com>
2024-12-02 22:53:37 +0400

    task07 update 3

--- task07/app/lambdas/uuid_generator/index.js
+++ task07/app/lambdas/uuid_generator/index.js
@@ -17,7 +17,7 @@ exports.handler = async (event) => {
         const startTime = new Date();
         const isoString = startTime.toISOString().replace('Z', '');
         const start = isoString.split('T')[0] + 'T';
-        const fileName = `${startTime.toISOString().replace('Z', '.000Z')}`;
+        const fileName = `${startTime.toISOString()}`;
 
         const params = {
             Bucket: bucketName,
