ecb894377d06d0557efa65d746a83dadcd800322
Hayk Dadyan <hayk_dadyan@epam.com>
2024-12-09 23:07:21 +0400

    task09 done

---
+++ task09/app/lambdas/processor/index.js
@@ -0,0 +1,54 @@
+// Import required AWS SDK and axios for HTTP requests
+const AWS = require('aws-sdk');
+const axios = require('axios');
+const xray = require('aws-xray-sdk');
+
+// Capturing all AWS clients
+xray.captureAWS(require('aws-sdk'));
+
+// Initialize DynamoDB Document Client
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+exports.handler = async (event) => {
+    const url = "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m";
+
+    try {
+        // Fetch data from Open-Meteo API
+        const response = await axios.get(url);
+        const weatherData = response.data;
+
+        if (!weatherData) {
+            return { statusCode: 400, body: 'No data received from API' };
+        }
+
+        const item = {
+            id: AWS.util.uuid.v4(), // Generate a random UUID for ID
+            forecast: {
+                elevation: weatherData.elevation,
+                generationtime_ms: weatherData.generationtime_ms,
+                hourly: weatherData.hourly,
+                hourly_units: weatherData.hourly_units,
+                latitude: weatherData.latitude,
+                longitude: weatherData.longitude,
+                timezone: weatherData.timezone,
+                timezone_abbreviation: weatherData.timezone_abbreviation,
+                utc_offset_seconds: weatherData.utc_offset_seconds
+            }
+        };
+
+        // Params object for DynamoDB
+        const params = {
+            TableName: 'cmtr-99c81425-Weather-test',
+            Item: item,
+        };
+
+        // Insert data into DynamoDB
+        await dynamoDb.put(params).promise();
+
+        // Return success response
+        return { statusCode: 200, body: JSON.stringify(item) };
+    } catch (error) {
+        console.error('Error fetching or storing data: ', error);
+        return { statusCode: 500, body: 'Failed to fetch or store data' };
+    }
+};
